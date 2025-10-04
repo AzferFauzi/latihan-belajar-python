d2f236c8e7c344b5edb8e8dc3e6073135e6a5bc2
Azfer Jausyaq Fauzi <mangvidch@gmail.com>
2025-10-04 12:57:50 +0700

    Create type_find_function.py
    Belajar Type Find Function di Python

---
+++ type_find_function.py
@@ -0,0 +1,18 @@
+# TYPE FIND DI FUNCTION
+
+# berfungsi untuk memproses sesuai tipe data
+
+# Type data integer
+
+def perhitungan(input:int) -> int:
+    hasil = input + input
+    return hasil
+
+print(perhitungan('lol'))
+
+# Type data string
+
+def data(inus: str):
+    print(f'Pesan : {inus}')
+
+data('lol')
